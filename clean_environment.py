aeee58d19e6bf5331d8911c462eb2cec9609b67f
or87600 <Or87600@gmail.com>
2023-01-27 01:34:19 +0200

    clean environment update

--- clean_environment.py
+++ clean_environment.py
@@ -1,4 +1,21 @@
 import requests
 
-requests.get('http://127.0.0.1:5000/stop_server')
-requests.get('http://127.0.0.1:5001/stop_server')
+from requests.exceptions import ChunkedEncodingError
+
+# Terminate REST API server
+try:
+    requests.get('http://127.0.0.1:5000/stop_server')
+    print("REST API SERVER terminated")
+except ChunkedEncodingError as conn_exce:
+    print("connection to REST API server probably already closed:", conn_exce)
+except Exception as exce:
+    print("Environment cleanup failed", exce)
+
+# Terminate web app server
+try:
+    requests.get('http://127.0.0.1:5001/stop_server')
+    print("Web app SERVER terminated")
+except ChunkedEncodingError as conn_exce:
+    print("connection to the Web-App server probably already closed:", conn_exce)
+except Exception as exce:
+    print("Environment cleanup failed", exce)
